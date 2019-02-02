160209ef69811f373be5a7a75314a37ba647c36c
Mihir Shah <mihir-ms>
2019-02-02 16:13:26 +0530

    add project

--- Client/src/components/Add Project/addproject.js
+++ Client/src/components/Add Project/addproject.js
@@ -1,50 +1,63 @@
-import React, { Component } from 'react';
-import {Grid, Row, Col, } from 'react-bootstrap';
-import Header from '../Header/header';
+import React, { Component } from "react";
+import Header from "../Header/header";
 import "./addproject.css";
+import {
+  Form,
+  FormGroup,
+  Col,
+  FormControl,
+  Button,
+  Checkbox,
+  ControlLabel,
+  InputGroup
+} from "react-bootstrap";
+
 class home extends Component {
-    render() { 
-        return ( 
-            
-            // <div><Header/><ProgressBar active now={45} /><ProgressBar>
-            // <ProgressBar striped bsStyle="success" now={35} key={1} />
-            // <ProgressBar bsStyle="warning" now={20} key={2} />
-            // <ProgressBar active bsStyle="danger" now={10} key={3} />
-            // </ProgressBar>;
-            // <div style={{ minheight: "100%"}}>
-            //   <Grid className="container-fluid" style={{ width: "100%" }}>
-            //     <Row>
-            //       <Col xs={4} className="button">
-            //         <Button className="btn btn-success">Primary</Button>{" "}
-            //       </Col>
-            //       <Col xs={4} className="button">
-            //         <Button className="btn btn-warning-outline">Primary</Button>{" "}
-            //       </Col>
-            //       <Col xs={4} className="button">
-            //       <Button className="btn btn-danger" outline={true}>
-            //         Primary
-            //       </Button>{" "}
-            //       </Col>
-            //     </Row>
-            //   </Grid>
-            // </div>
-            // </div>
-            <div style={{minHeight:'100%'}}>
-            <div class="headerclass">
-            <Header/>
-            </div>
-            <div className="bodyclass">
-              <Grid>
-                <Row>
-                  <Col lg={{span:4, offset:6}}>
-                    <a href={'carousel'}><h1>add project</h1></a>
-                  </Col>
-                </Row>
-              </Grid>
-            </div>
-            </div>
-         );
-    }
+  render() {
+    return (
+      <div style={{ minHeight: "100%" }}>
+        <div class="headerclass">
+          <Header />
+        </div>
+        <div className="bodyclass">
+          <Form horizontal>
+            <FormGroup controlId="formHorizontalEmail">
+              <Col componentClass={ControlLabel} sm={2}>
+                Project Name
+              </Col>
+              <Col sm={8}>
+                <FormControl type="text" placeholder="" />
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formHorizontalEmail">
+              <Col componentClass={ControlLabel} sm={2}>
+                Cost
+              </Col>
+              <Col sm={1}>
+                <FormControl type="text" placeholder="" />
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formHorizontalPassword">
+              <Col componentClass={ControlLabel} sm={2}>
+                Description
+              </Col>
+              <Col sm={8}>
+              <FormControl as="textarea" aria-label="With textarea" />
+              </Col>
+            </FormGroup>
+
+            <FormGroup>
+              <Col smOffset={2} sm={10}>
+                <Button type="submit">Add Project</Button>
+              </Col>
+            </FormGroup>
+          </Form>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default home;
