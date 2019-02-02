ecd208ba9b91bb58f8cb595612ef397944fed6dc
Priyam <priyam.ds@somaiya.edu>
2019-02-02 22:04:56 +0530

    add project

---
+++ Client/build/precache-manifest.15a5302ad1447bcc967894108bacc04c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aada70ae94c6842641ba",
+    "url": "/static/js/main.aada70ae.chunk.js"
+  },
+  {
+    "revision": "ace3a009fec5677f4257",
+    "url": "/static/js/1.ace3a009.chunk.js"
+  },
+  {
+    "revision": "aada70ae94c6842641ba",
+    "url": "/static/css/main.a45d1a8e.chunk.css"
+  },
+  {
+    "revision": "ace3a009fec5677f4257",
+    "url": "/static/css/1.b6f59751.chunk.css"
+  },
+  {
+    "revision": "0e93cb87e516363b0cae729530fad487",
+    "url": "/index.html"
+  }
+];
--- Client/src/components/Add Project/addproject.js
+++ Client/src/components/Add Project/addproject.js
@@ -1,50 +1,112 @@
-import React, { Component } from 'react';
-import {Grid, Row, Col, } from 'react-bootstrap';
-import Header from '../Header/header';
+import React, { Component } from "react";
+import Header from "../Header/header";
 import "./addproject.css";
-class home extends Component {
-    render() { 
-        return ( 
-            
-            // <div><Header/><ProgressBar active now={45} /><ProgressBar>
-            // <ProgressBar striped bsStyle="success" now={35} key={1} />
-            // <ProgressBar bsStyle="warning" now={20} key={2} />
-            // <ProgressBar active bsStyle="danger" now={10} key={3} />
-            // </ProgressBar>;
-            // <div style={{ minheight: "100%"}}>
-            //   <Grid className="container-fluid" style={{ width: "100%" }}>
-            //     <Row>
-            //       <Col xs={4} className="button">
-            //         <Button className="btn btn-success">Primary</Button>{" "}
-            //       </Col>
-            //       <Col xs={4} className="button">
-            //         <Button className="btn btn-warning-outline">Primary</Button>{" "}
-            //       </Col>
-            //       <Col xs={4} className="button">
-            //       <Button className="btn btn-danger" outline={true}>
-            //         Primary
-            //       </Button>{" "}
-            //       </Col>
-            //     </Row>
-            //   </Grid>
-            // </div>
-            // </div>
-            <div style={{minHeight:'100%'}}>
-            <div class="headerclass">
-            <Header/>
-            </div>
-            <div className="bodyclass">
-              <Grid>
-                <Row>
-                  <Col lg={{span:4, offset:6}}>
-                    <a href={'carousel'}><h1>add project</h1></a>
-                  </Col>
-                </Row>
-              </Grid>
-            </div>
-            </div>
-         );
+import {Form, FormGroup,Col,FormControl,Button,Checkbox,ControlLabel,Grid, Row,} from 'react-bootstrap';
+import web3 from '../../web';
+import {abi,address} from '../../project_contract';
+import { Redirect } from 'react-router-dom';
+let contract;
+
+class addproject extends Component {
+
+  state={
+    isMetaMask:'',
+
     }
-}
- 
-export default home;
+    
+    async componentDidMount(){
+    
+    if (typeof window.web3 !== 'undefined') {
+    console.log('web3 is enabled');
+    if (web3.currentProvider.isMetaMask === true) {
+    await this.setState({isMetaMask: true});
+    console.log('MetaMask is active');
+    } else {
+    console.log('MetaMask is not available')
+    }
+    } else {
+    console.log('web3 is not found')
+    }
+    if(this.state.isMetaMask){
+    console.log("metamask done");
+    }
+    
+    }
+    addProject= async(event)=>{
+      contract = new web3.eth.Contract(abi, address);
+      console.log(contract);
+      event.preventDefault();
+      const data= new FormData(event.target);
+      const pname=data.get("pname");
+      const pdescription=data.get("pdescription");
+      const pcost=data.get("pcost");
+      const accounts = await web3.eth.getAccounts();
+      await(contract.methods.AddProject(pcost,pname,pdescription).send({
+          "from":accounts[0],
+      })
+      );
+      
+      console.log(pname + pdescription + pcost);
+    }
+
+
+    
+      render() { 
+          return ( 
+              <div style={{minHeight:'100%'}}>
+              <div class="headerclass">
+              <Header/>
+              </div>
+  
+              <div className="bodyclass">
+  
+                <Grid>
+                  <Row>
+                    <Col lg={{span:4, offset:6}}>
+                      <a href={'carousel'}><h1>Hii</h1></a>
+                      <form onSubmit={this.addProject}>
+                      <Form horizontal>
+                        <FormGroup controlId="formHorizontalEmail">
+                          <Col componentClass={ControlLabel} sm={2}>
+                                Project Name
+                          </Col>
+                          <Col sm={10}>
+                            <FormControl type="text" name="pname" placeholder="Project Name" />
+                          </Col>
+                        </FormGroup>
+                        <FormGroup controlId="formHorizontalEmail">
+                          <Col componentClass={ControlLabel} sm={2}>
+                            Project Description
+                          </Col>
+                          <Col sm={10}>
+                            <FormControl type="text" name="pdescription" placeholder="Project Description" />
+                          </Col>
+                        </FormGroup>
+                        <FormGroup controlId="formHorizontalEmail">
+                          <Col componentClass={ControlLabel} sm={2}>
+                            Project Cost
+                          </Col>
+                          <Col sm={10}>
+                            <FormControl type="text" name="pcost" placeholder="1.0 Ether" />
+                          </Col>
+                        </FormGroup>
+                        <FormGroup>
+                          <Col smOffset={2} sm={10}>
+                            <Button type="submit" name="submit" >Add Project</Button>
+                          </Col>
+                        </FormGroup>
+                      </Form>
+                      </form>
+                    </Col>
+                  </Row>
+                </Grid>
+              </div>
+              </div>
+           );
+      }
+
+
+
+  }
+   
+  export default addproject;
