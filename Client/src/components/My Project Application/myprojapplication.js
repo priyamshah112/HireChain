800ed65373f547d83c5c5f75e474193cabb810eb
Priyam <priyam.ds@somaiya.edu>
2019-02-03 06:29:03 +0530

    myapplication

--- Client/src/components/My Project Application/myprojapplication.js
+++ Client/src/components/My Project Application/myprojapplication.js
@@ -1,9 +1,137 @@
 import React, { Component } from "react";
-import { Grid, Row, Col,Button } from "react-bootstrap";
+import { Grid, Row, Col,Button, Glyphicon,FormGroup ,FormControl,ControlLabel,Form} from "react-bootstrap";
 import Header from "../Header/header";
+import {abi,address} from "../../project_contract";
+import {abi2,address2} from "../../user_contarct";
+
+//import { Card, CardTitle, CardActions, CardText } from "react-mdl";
 import "./myprojapplication.css";
+import web3 from "../../web";
+let contract;
+let userContract;
 class myprojapplication extends Component {
+  state={
+    projects:[],
+    
+  }
+
+  async componentDidMount(){
+    if(web3.currentProvider.isMetaMask===true){
+      contract=new web3.eth.Contract(abi,address);
+      userContract=new web3.eth.Contract(abi2,address2);
+      const accounts=await web3.eth.getAccounts();
+      const account=accounts[0];
+      const userNum=await userContract.methods.countUsers().call();
+
+      console.log(userNum)
+      var projects=[]
+      for(var j=0;j<userNum;j++){
+        const useraddr=await userContract.methods.Users(j).call();
+        console.log(useraddr);
+
+        const numberProjects=await contract.methods.lenC().call({
+          "from":useraddr
+        });
+        console.log(numberProjects);
+
+        if(numberProjects!=0){
+          for(var i =0;i<numberProjects;i++){
+            
+            const projectres=await(contract.methods.ProjectMap(useraddr,i)).call();
+            var dic={};
+            dic[useraddr]=projectres;
+            projects.push(dic);
+            var nam = projectres[0];
+            console.log(nam,"dic");
+            // var strin = "/api/v1/auction/"+nam;
+            // console.log(strin);
+            // const respon= await fetch(strin);
+            // var data = await respon.json();
+            // console.log(data,"my response");
+            var response =await fetch(`/api/v1/auction/HireChain`);
+            // console.log(response.auction.user_bid);
+            var data=await response.json();
+            console.log(data.auction[0]);
+          }
+        }
+
+        
+        
+      }
+      this.setState({projects:projects});
+      console.log(this.state.projects);
+      // for(var i=0;i<numberProjects)
+
+
+    }
+  }
+
+  Submit=async(event)=>{
+    event.preventDefault();
+    const data= new FormData(event.target);
+    const link=data.get("link");
+    const accounts=await web3.eth.getAccounts();
+    const account=accounts[0];
+    console.log("bid:",link);
+    const response= await fetch(link);
+    console.log(response);
+  }
+
   render() {
+
+    const val=this.state.projects;
+    // console.log(val);
+    var projects=[]
+    val.forEach(element => {
+      console.log(element);
+      for(var key in element){
+        // console.log();
+        var projectname=element[key].pname;
+        
+        projects.push(
+                    <Grid>
+                    <Row style={{left:"-10px"}}> 
+                      <Col xs={12}>
+                        <div className="cards">
+                          <div className="card">
+                          <Grid>
+                          <Row className="cardHeader">
+                            <Col class="Headershift" xs={10}>
+                              <h5>&nbsp;&nbsp; {projectname}</h5>
+                            </Col>
+                           < Col xs={2} >
+                             <Form onSubmit={this.Bid}>
+                              <FormGroup controlId="formHorizontalText">
+                                  <Col sm={10}>
+                                  <FormControl type="hidden" name="pname" id="pname" value={projectname}/>
+                                  <FormControl type="text" id="bid" name="bid" placeholder="Bid" />
+                                  
+                                </Col>
+                            </FormGroup>
+                            <Col xs={6}>
+                            <Button type="submit" className="btn btn-success"><span class="glyphicon glyphicon-ok">&nbsp;</span>Apply</Button>{" "}
+                            </Col>
+                            </Form>   
+                             </Col>
+                          </Row>
+                          <Row className="cardDesc">
+                              <Col xs={10}>
+                                <p>Cost 
+                                </p>
+                              </Col>
+                            
+                          </Row>
+                          </Grid>
+                            <div className="front">
+                            </div>
+                          </div>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Grid>
+        )
+      }
+    });
     return (
       // <div><Header/><ProgressBar active now={45} /><ProgressBar>
       // <ProgressBar striped bsStyle="success" now={35} key={1} />
@@ -28,46 +156,29 @@ class myprojapplication extends Component {
       //   </Grid>
       // </div>
       // </div>
-      <div style={{ minHeight: "100%" }}>
+      <div style={{ minHeight: "100%", width:"120%" }}>
         <div class="headerclass">
           <Header />
+          <Grid style={{width:"100%"}} >
+                    <Row style={{background:"rgb(47, 53, 58)", width:"100%"}}>
+                        <Col xs={4} xsOffset={4}>
+                        <br/>
+                        <p className="username">Dashboard</p>
+                        </Col>
+                        <Col xs={2} xsOffset={1} style={{background:"rgb(47, 53, 58)", height:"50px", left:"0"}}>
+                        <br/>  <p className="username"><Glyphicon glyph="user" />&nbsp;&nbsp;Hello User</p>
+                        </Col>
+                    </Row>
+                </Grid>
         </div>
         <div className="bodyclass">
-          <Grid>
-            <Row>
-              <Col lg={{ span: 4, offset: 6 }}>
-              <div className="cards">
-                  <div className="card">
-                  <Grid>
-                  <Row className="cardHeader">
-                    <Col class="Headershift">
-                      <h5>&nbsp;&nbsp; Project Name</h5>
-                    </Col>
-                  </Row>
-                  <Row className="cardDesc">
-                      <Col xs={10}>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        
+          {projects}
 
-Why do we use it?
-ipsum dolor sit amet...'
-</p>
-                      </Col>
-                      <Col xs={1}>
-                      <Button className="btn btn-success"><span class="glyphicon glyphicon-ok">&nbsp;</span>Apply</Button>{" "}
-                      </Col>
-                  </Row>
-                  </Grid>
-                    <div className="front">
-                    </div>
-                  </div>
-                </div>
-              </Col>
-            </Row>
-          </Grid>
         </div>
       </div>
     );
   }
 }
-
+ 
 export default myprojapplication;
